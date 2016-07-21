8d86781bb1024faf576321af4864b6e3d378ca8c
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-07-21 15:18:21 +0200

    migrate build to new linting

--- build-js.sh
+++ build-js.sh
@@ -2,13 +2,19 @@
 BASEDIR=`pwd`
 cd src/tao/views/build
 export PATH="$PATH:/var/lib/jenkins/.nvm/versions/node/v6.2.1/bin"
+
+node --version
 npm --version
 sass --version
-grunt --version
+
 echo "Install NPM"
 npm --no-color install
+
 echo "Compile JS"
 npm run bundle        
+
 echo "Check SASS runtime"
 npm run sass
-grunt testall --no-color --force --reports=$BASEDIR/reports
+
+echo "Run js unit test"
+npm run test -- --reports=$BASEDIR/reports
